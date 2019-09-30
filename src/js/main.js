50523616d438e1a64903ace88475b220b6121774
Hilary Fung <hfung@seattletimes.com>
2019-09-30 15:50:07 -0700

    add a quote, use start button, style buttons

--- src/js/main.js
+++ src/js/main.js
@@ -22,10 +22,9 @@ var options = Object.keys(window.data);
 
 qsa(".goto").forEach(function(btn) {
 	btn.addEventListener("click", function(e) {
-
 		var selected = document.querySelector(".selected");
-
 		var selectedIndex = selected ? options.indexOf(selected.id) : -1;
+
 		if (e.target.classList.contains("back")) {
 			selectedIndex = Math.max(0, selectedIndex - 1);
 		} else {
@@ -33,12 +32,15 @@ qsa(".goto").forEach(function(btn) {
 		}
 
 		$(".goto.back").toggleClass("disabled", selectedIndex == 0);
-    $(".goto.next").toggleClass("disabled", selectedIndex == options.length - 1);
+   	$(".goto.next").toggleClass("disabled", selectedIndex == options.length - 1);
 
 		document.querySelector(".details").innerHTML = template(data[options[selectedIndex]]);
     if (document.querySelector(".selected")) document.querySelector(".selected").classList.remove("selected");
-    document.querySelector("#" + options[selectedIndex]).classList.add("selected");
+    	document.querySelector("#" + options[selectedIndex]).classList.add("selected");
 	});
 });
 
-
+document.querySelector(".start").addEventListener("click", function(e) {
+	console.log("starting")
+	$(".buttons").addClass("shown");
+});
