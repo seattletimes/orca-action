02a37020d0e1e9c7a3a16e1fccaa8bb31161d3f8
Hilary Fung <hfung@seattletimes.com>
2019-10-04 17:06:01 -0700

    switch to data attributes to (attempt to) support IE11

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,3 @@
-// require("./lib/social");
-// require("./lib/ads");
-// var track = require("./lib/tracking");
-
 require("component-responsive-frame/child");
 var dot = require("./lib/dot");
 var $ = require("jquery");
@@ -10,45 +6,83 @@ var template = dot.compile(require("./_info.html"));
 
 var qsa = s => Array.prototype.slice.call(document.querySelectorAll(s));
 
-qsa(".st0").forEach(function(group) {
-  group.addEventListener("click", function(e) {
-  	$(".buttons").addClass("shown");
-    document.querySelector(".details").innerHTML = template(data[e.target.classList[1]]);
-    if (document.querySelector(".selected")) 
-    	document.querySelector(".selected").classList.remove("selected");
-    e.target.classList.add("selected");
-  });
-});
+var addClass = function(elm, cssClass) {
+	if (!!elm.classList) {
+		elm.classList.add(cssClass);
+	} else if (elm.nodeName === "SVG") {
+		let appliedClasses = elm.getAttribute("class") || "";
+		appliedClasses = !appliedClasses.split(" ").includes(cssClass)
+			? `${elm.className} ${cssClass}`
+			: appliedClasses;
+      	elm.setAttribute('class', appliedClasses);
+	}
+};
 
-var options = Object.keys(window.data);
+$(document).ready(function() {
+	qsa(".st0").forEach(function(bubble) {
+	  bubble.addEventListener("click", function(e) {
+	  	$(".buttons").addClass("shown");
+	    document.querySelector(".details").innerHTML = template(data[e.target.dataset.key]);
+	    
+	    // "selected" class makes bubble gray while selected
+	    if (document.querySelector(".selected")) {
+	    	document.querySelector(".selected").classList.remove("selected");
+	    }
+	    e.target.classList.add("selected");
 
-qsa(".goto").forEach(function(btn) {
-	btn.addEventListener("click", function(e) {
-		var selected = document.querySelector(".selected");
-		var selectedIndex = selected ? options.indexOf(selected.classList[1]) : -1;
-
-		if (e.target.classList.contains("back")) {
-			selectedIndex = Math.max(0, selectedIndex - 1);
-		} else {
-			selectedIndex = Math.min(options.length - 1, selectedIndex + 1);
-		}
-		$(".goto.back").toggleClass("disabled", selectedIndex == 0);
-   	$(".goto.next").toggleClass("disabled", selectedIndex == options.length - 1);
-
-		document.querySelector(".details").innerHTML = template(data[options[selectedIndex]]);
-    if (document.querySelector(".selected"))
-    	qsa(".selected").forEach(function(bubble) {
-    		bubble.classList.remove("selected")
-    	});
-    	qsa("." + options[selectedIndex]).forEach(function(bubble) {
-    		bubble.classList.add("selected")
-    	});
+	    // "selected" data attribute for functionality; supports IE 11
+	    document.querySelectorAll(".st0").forEach(function(bubble) {
+	    	bubble.dataset.selected = "false";
+	    });
+	    e.target.dataset.selected = "true";
+	  });
 	});
-});
 
-document.querySelector(".start").addEventListener("click", function(e) {
-	qsa(".personal").forEach(function(bubble) {
-		bubble.classList.add("selected")
-	})
-	$(".buttons").addClass("shown");
+	var options = Object.keys(window.data);
+
+	qsa(".goto").forEach(function(btn) {
+		btn.addEventListener("click", function(e) {
+			var selected = $("*[data-selected='true']")[0];
+			var selectedIndex = selected ? options.indexOf(selected.dataset.key) : -1;
+
+			if (e.target.classList.contains("back")) {
+				selectedIndex = Math.max(0, selectedIndex - 1);
+			} else {
+				selectedIndex = Math.min(options.length - 1, selectedIndex + 1);
+			}
+
+			$(".goto.back").toggleClass("disabled", selectedIndex == 0);
+	   	$(".goto.next").toggleClass("disabled", selectedIndex == options.length - 1);
+
+			document.querySelector(".details").innerHTML = template(data[options[selectedIndex]]);
+	    
+			var new_key = options[selectedIndex];
+
+			// "selected" class to make bubble gray while selected
+	    if (document.querySelector(".selected")) {
+	    	qsa(".selected").forEach(function(bubble) {
+	    		bubble.classList.remove("selected")
+	    	});
+	    }
+    	$("*[data-key=" + new_key + "]").addClass("selected");
+
+    	// "selected" data attribute for functionality; supports IE 11
+	    document.querySelectorAll(".st0").forEach(function(bubble) {
+	    	bubble.dataset.selected = "false";
+	    });
+			$("*[data-key=" + new_key + "]").each(function() {
+				this.dataset.selected = "true";
+			})
+		});
+	});
+
+	document.querySelector(".start").addEventListener("click", function(e) {
+		$("*[data-key='personal']").each(function() {
+			this.classList.add("selected");
+			this.dataset.selected = "true";
+		})
+		$(".buttons").addClass("shown");
+
+		document.querySelector(".details").innerHTML = template(data[options[0]]);
+	});
 });
